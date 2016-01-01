74797c3156dd1c7972a45d4b98ca655e274a18e6
Jaakko Laurikainen <lkaino@gmail.com>
2016-01-01 14:57:21 +0200

    Tailtune V2
    Improved tail tune: faster and more accurate.
    Tail tune in unarmed state allows setting up
    servo min, mid and max positions.
    
    Servo feedback support.

--- src/main/main.c
+++ src/main/main.c
@@ -352,7 +352,7 @@ void init(void)
     adc_params.enableVBat = feature(FEATURE_VBAT);
     adc_params.enableRSSI = feature(FEATURE_RSSI_ADC);
     adc_params.enableCurrentMeter = feature(FEATURE_CURRENT_METER);
-    adc_params.enableExternal1 = false;
+    adc_params.enableExternal1 = true;
 #ifdef OLIMEXINO
     adc_params.enableExternal1 = true;
 #endif
