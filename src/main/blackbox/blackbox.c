b1a2eea66e7285c60830ab27fdf940bd5152e150
Jaakko Laurikainen <lkaino@gmail.com>
2016-01-08 23:43:06 +0200

    Fix blackbox inflight adjustments.

--- src/main/blackbox/blackbox.c
+++ src/main/blackbox/blackbox.c
@@ -1174,6 +1174,7 @@ void blackboxLogEvent(FlightLogEvent event, flightLogEventData_t *data)
                 blackboxWrite(data->inflightAdjustment.adjustmentFunction);
                 blackboxWriteSignedVB(data->inflightAdjustment.newValue);
             }
+        break;
         case FLIGHT_LOG_EVENT_GTUNE_RESULT:
             blackboxWrite(data->gtuneCycleResult.gtuneAxis);
             blackboxWriteSignedVB(data->gtuneCycleResult.gtuneGyroAVG);
